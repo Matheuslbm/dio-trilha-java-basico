24305c25d3d03fb72574a9b93a621ef2963c99bc
MatheusLima <matheusslimabm@gmail.com>
2023-10-03 12:44:12 -0300

    Inserindo operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,28 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        int a, b ;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        
+        System.out.println(resultado);
+
+        /* String resultado ="";
+        if(a==b)
+          resultado = "verdadeiro";
+        else
+          resultado = "falso";
+        System.out.println(resultado); */ 
+         
+
+    }
+
+
+
+
+}
